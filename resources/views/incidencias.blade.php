b9ae1771b85d9be9175d288b6dbc498b267ccdf2
Daniel Galvan <dalegalvan15@gmail.com>
2020-04-15 21:36:31 +0200

    blades

---
+++ resources/views/incidencias.blade.php
@@ -0,0 +1,9 @@
+@extends('templates.main')
+
+@section('titulo')
+    Gestion Incidencias
+@endsection
+
+@section('principal')
+    <registro-incidencia></registro-incidencia>
+@endsection
---
+++ resources/views/recursos.blade.php
@@ -0,0 +1,9 @@
+@extends('templates.main')
+
+@section('titulo')
+    Gestion Recursos
+@endsection
+
+@section('principal')
+    <gestion-recursos></gestion-recursos>
+@endsection
