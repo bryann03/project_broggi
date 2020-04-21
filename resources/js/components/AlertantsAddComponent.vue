fff4d63dda979ef65241f06c8f0d3d1af143c293
Pau Gonzàlez Martí <lazytime96@gmail.com>
2020-04-21 23:36:01 +0200

    Afegits els idiomes als alertants

--- resources/js/components/AlertantsAddComponent.vue
+++ resources/js/components/AlertantsAddComponent.vue
@@ -1,77 +1,104 @@
 <template>
-    <main>
-        <div class="card mt-5">
-            
-            <div class="card-header">NOU ALERTANT</div>
-            
-            <div class="card-body">
+  <main>
+    <div class="card mt-5">
+      <div class="card-header">NOU ALERTANT</div>
 
-                <b-form method="post">
-
-                    <!-- Nom -->
-                    <div class="form-group row">
-                        <label for="name" class="col-sm-1 col-form-label">Nom</label>
-                        <div class="col-sm-11">
-                            <input type="text" name="nom" id="nom" class="form-control" placeholder="Nom de l'alertant" v-model="alertant.nom">
-                        </div>
-                    </div>
-
-                    <!-- Adreça -->
-                    <div class="form-group row">
-                        <label for="tel" class="col-sm-1 col-form-label">Adreça</label>
-                        <div class="col-sm-11">
-                            <input type="text" name="adreca" id="adreca" class="form-control" placeholder="Adreça de l'alertant" v-model="alertant.adreca">
-                        </div>
-                    </div>
-
-                    <!-- Telefon -->
-                    <div class="form-group row">
-                        <label for="tel" class="col-sm-1 col-form-label">Telèfon</label>
-                        <div class="col-sm-11">
-                            <input type="text" name="tel" id="tel" class="form-control" placeholder="Telèfon de l'alertant" v-model="alertant.tel">
-                        </div>
-                    </div>
-
-                    <!-- Submit -->
-                    <div class="form-group row">
-                        <div class="col-sm-11 offset-1">
-                            <b-button class="btn btn-secondary" @click="insertAlertant()">AFEGIR</b-button>
-                        </div>
-                    </div>
-
-                </b-form>
-
+      <div class="card-body">
+        <b-form method="post">
+          <!-- Nom -->
+          <div class="form-group row">
+            <label for="name" class="col-sm-1 col-form-label">{{textos.nom[idioma]}}</label>
+            <div class="col-sm-11">
+              <input
+                type="text"
+                name="nom"
+                id="nom"
+                class="form-control"
+                placeholder="Nom de l'alertant"
+                v-model="alertant.nom"
+              />
             </div>
+          </div>
 
-        </div>
-    </main>
+          <!-- Adreça -->
+          <div class="form-group row">
+            <label for="tel" class="col-sm-1 col-form-label">{{textos.adreca[idioma]}}</label>
+            <div class="col-sm-11">
+              <input
+                type="text"
+                name="adreca"
+                id="adreca"
+                class="form-control"
+                placeholder="Adreça de l'alertant"
+                v-model="alertant.adreca"
+              />
+            </div>
+          </div>
+
+          <!-- Telefon -->
+          <div class="form-group row">
+            <label for="tel" class="col-sm-1 col-form-label">{{textos.telefon[idioma]}}</label>
+            <div class="col-sm-11">
+              <input
+                type="text"
+                name="tel"
+                id="tel"
+                class="form-control"
+                placeholder="Telèfon de l'alertant"
+                v-model="alertant.tel"
+              />
+            </div>
+          </div>
+
+          <!-- Submit -->
+          <div class="form-group row">
+            <div class="col-sm-11 offset-1">
+              <b-button
+                class="btn btn-secondary"
+                @click="insertAlertant()"
+              >{{textos.afegir[idioma]}}</b-button>
+            </div>
+          </div>
+        </b-form>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
-    export default {
-        data () {
-            return {
-                alertant: {
-                    "nom": "",
-                    "adreca": "",
-                    "tel": "",
-                }
-            }
-        },
-        methods: {
-            insertAlertant() {
-                let me = this;
+export default {
+  data() {
+    return {
+      idioma: 0,
+      textos: {
+        nom: ["Nom", "Nombre", "Name"],
+        adreca: ["Adreça", "Dirección", "Address"],
+        telefon: ["Telèfon", "Telefono", "Phone"],
+        afegir: ["Afegir", "Añadir", "Add"]
+      },
+      alertant: {
+        nom: "",
+        adreca: "",
+        tel: ""
+      }
+    };
+  },
+  methods: {
+    insertAlertant() {
+      let me = this;
 
-                axios.post("/alertants", me.alertant).then(function (response) {
-                    me.alertant = response.data;
-                })
-                .catch (function (error) {
-                    console.log(error);
-                })
-            },
-        },
-        mounted() {
-            console.log('Component mounted.')
-        }
+      axios
+        .post("/alertants", me.alertant)
+        .then(function(response) {
+          me.alertant = response.data;
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
     }
+  },
+  mounted() {
+    console.log("Component mounted.");
+  }
+};
 </script>
--- resources/js/components/AlertantsComponent.vue
+++ resources/js/components/AlertantsComponent.vue
@@ -1,147 +1,212 @@
 <template>
-    <main>
-        <form action="" method="get" class="form-horizontal">
-            <div class="form-group row mt-5 align-middle">
-                <div class="col-10">
-                    <b-form-input v-model="filter" type="search" id="filterInput" placeholder="Búsqueda ..."></b-form-input>
-                </div>
-                <button type="submit" class="btn btn-secondary btn-sm col-2">BUSCAR</button>
-            </div>
-        </form>
+  <main>
+    <form action method="get" class="form-horizontal">
+      <div class="form-group row mt-5 align-middle">
+        <div class="col-10">
+          <b-form-input
+            v-model="filter"
+            type="search"
+            id="filterInput"
+            :placeholder="textos.buscar[idioma] + ' ...'"
+          ></b-form-input>
+        </div>
+        <button type="submit" class="btn btn-secondary btn-sm col-2">{{textos.buscar[idioma]}}</button>
+      </div>
+    </form>
 
-        <b-table striped hover id="table_alertants" :items="alertants" :fields="fields" class="table mt-5" :per-page="perPage" :current-page="currentPage" :filter="filter">
-            <template v-slot:cell(gestionar)="row">
-                <b-button size="sm" class="mr-1" @click="editAlertant(row.item)">
-                    Editar
-                </b-button>
-                <b-button size="sm" @click="deleteAlertant(row.item)">
-                    Eliminar
-                </b-button>
-            </template>
-        </b-table>
-        <b-pagination v-model="currentPage" :total-rows="rows" :per-page="perPage" aria-controls="table_alertants"></b-pagination>
+    <b-table
+      striped
+      hover
+      id="table_alertants"
+      :items="alertants"
+      :fields="fields"
+      class="table mt-5"
+      :per-page="perPage"
+      :current-page="currentPage"
+      :filter="filter"
+    >
+      <template v-slot:cell(gestionar)="row">
+        <b-button size="sm" class="mr-1" @click="editAlertant(row.item)">{{textos.editar[idioma]}}</b-button>
+        <b-button size="sm" @click="deleteAlertant(row.item)">{{textos.eliminar[idioma]}}</b-button>
+      </template>
+    </b-table>
+    <b-pagination
+      v-model="currentPage"
+      :total-rows="rows"
+      :per-page="perPage"
+      aria-controls="table_alertants"
+    ></b-pagination>
 
-        <!-- Info modal -->
-        <b-modal :id="editAlertantModal.id" :title="editAlertantModal.title" @ok="saveAlertant()" :ok-title="aceptar" :cancel-title="cancelar">
-            <input type="hidden" name="_method" value="PUT">
-            <!--<pre>{{ editAlertantModal.content }}</pre>-->
-            <div class="form-group">
-                <label for="exampleFormControlInput1">Nom:</label>
-                <b-form-input id="nom" name="nom" class="form-control" placeholder="Nom" :value="editAlertantModal.info.name" v-model="editAlertantModal.info.name"></b-form-input>
-            </div>
+    <!-- Info modal -->
+    <b-modal
+      :id="editAlertantModal.id"
+      :title="editAlertantModal.title"
+      @ok="saveAlertant()"
+      :ok-title="textos.aceptar[idioma]"
+      :cancel-title="textos.cancelar[idioma]"
+    >
+      <input type="hidden" name="_method" value="PUT" />
+      <!--<pre>{{ editAlertantModal.content }}</pre>-->
+      <div class="form-group">
+        <label for="exampleFormControlInput1">{{textos.nom[idioma]}}</label>
+        <b-form-input
+          id="nom"
+          name="nom"
+          class="form-control"
+          :placeholder="textos.nom[idioma]"
+          :value="editAlertantModal.info.name"
+          v-model="editAlertantModal.info.name"
+        ></b-form-input>
+      </div>
 
-            <div class="form-group">
-                <label for="exampleFormControlInput1">Adreça:</label>
-                <b-form-input id="adreca" name="adreca" class="form-control" placeholder="Adreça" :value="editAlertantModal.info.adress" v-model="editAlertantModal.info.adress"></b-form-input>
-            </div>
+      <div class="form-group">
+        <label for="exampleFormControlInput1">{{textos.adreca[idioma]}}</label>
+        <b-form-input
+          id="adreca"
+          name="adreca"
+          class="form-control"
+          :placeholder="textos.adreca[idioma]"
+          :value="editAlertantModal.info.adress"
+          v-model="editAlertantModal.info.adress"
+        ></b-form-input>
+      </div>
 
-            <div class="form-group">
-                <label for="exampleFormControlInput1">Telefon:</label>
-                <b-form-input  id="telefon" name="telefon" class="form-control" placeholder="Telefon" :value="editAlertantModal.info.tel" v-model="editAlertantModal.info.tel"></b-form-input>
-            </div>
-        </b-modal>
-    </main>
+      <div class="form-group">
+        <label for="exampleFormControlInput1">{{textos.telefon[idioma]}}</label>
+        <b-form-input
+          id="telefon"
+          name="telefon"
+          class="form-control"
+          :placeholder="textos.telefon[idioma]"
+          :value="editAlertantModal.info.tel"
+          v-model="editAlertantModal.info.tel"
+        ></b-form-input>
+      </div>
+    </b-modal>
+  </main>
 </template>
 
 <script>
-    export default {
-        data () {
-            return {
-                alertants: [],
-                alertant: {
-                    id: null,
-                    nom: "",
-                    cognoms: "",
-                    adreca: "",
-                    municipis_id: null,
-                    telefon: "",
-                    tipus_alertant_id: ''
-                },
-                perPage: 5,
-                currentPage: 1,
-                fields: [
-                    {
-                        key: 'nom',
-                    },
-                    {
-                        key: 'adreca',
-                        label: 'Adreça',
-                    },
-                    {
-                        key: 'telefon',
-                        label: 'Telèfon',
-                    },
-                    "gestionar"
-                ],
-                editAlertantModal: {
-                    id: 'editAlertantModal',
-                    title: 'EDITAR ALERTANTE',
-                    info: {
-                        name: "",
-                        adress: "",
-                        tel: ""
-                    }
-                },
-                currentAlertant : [],
-                filter: null,
-                aceptar : "Aceptar",
-                cancelar : "Cancelar"
-            }
+export default {
+  data() {
+    return {
+      idioma: 0,
+      textos: {
+        nom: ["Nom:", "Nombre:", "Name:"],
+        adreca: ["Adreça:", "Dirección:", "Address:"],
+        telefon: ["Telèfon:", "Telefono:", "Phone:"],
+        editar: ["Editar", "Editar", "Edit"],
+        eliminar: ["Eliminar", "Eliminar", "Remove"],
+        aceptar: ["Acceptar", "Aceptar", "Accept"],
+        cancelar: ["Cancelar", "Cancelar", "Cancel"],
+        buscar: ["Buscar", "Buscar", "Search"],
+        editarAlertant: ["Editar Alertant", "Editar Alertante", "Edit Alerting"]
+      },
+      alertants: [],
+      alertant: {
+        id: null,
+        nom: "",
+        cognoms: "",
+        adreca: "",
+        municipis_id: null,
+        telefon: "",
+        tipus_alertant_id: ""
+      },
+      perPage: 5,
+      currentPage: 1,
+      fields: [
+        {
+          key: "nom",
+          //   {{textos.nom[idioma]}}
+          lavel: "Nom"
         },
-        created() {
-            this.showAlertants();
+        {
+          key: "adreca",
+          //   {{textos.adreca[idioma]}}
+          label: "Adreça"
         },
-        computed: {
-            rows() {
-                return this.alertants.length
-            }
+        {
+          key: "telefon",
+          //   {{textos.telefon[idioma]}}
+          label: "Telèfon"
         },
-        methods: {
-            showAlertants() {
-                let me = this;
-
-                axios.get("/alertants").then(function (response) {
-                    me.alertants = response.data;
-                })
-                .catch (function (error) {
-                    console.log(error);
-                })
-            },
-
-            deleteAlertant(item) {
-                let me = this;
-                axios.delete("/alertants/" + item.id).then(function (response) {
-                    me.showAlertants();
-                })
-                .catch (function (error) {
-                    me.missatge = error.response.data;
-                    me.mensajesError.push(me.missatge.error)
-            })},
-
-            editAlertant(item){
-                let me = this;
-                me.currentAlertant = item;
-
-                this.editAlertantModal.info.name = item.nom;
-                this.editAlertantModal.info.adress = item.adreca;
-                this.editAlertantModal.info.tel = item.telefon;
-                this.$bvModal.show('editAlertantModal');
-            },
-
-            saveAlertant(){
-                let me = this;
-                
-                axios.put("/alertants/" + me.currentAlertant.id, me.editAlertantModal.info).then(function (response) {
-                    me.showAlertants();
-                })
-                .catch (function (error) {
-                    me.missatge = error.response.data;
-                    me.mensajesError.push(me.missatge.error)
-                })
-            }
-        },
-        mounted() {
-            console.log('Component mounted.')
+        "gestionar"
+      ],
+      editAlertantModal: {
+        id: "editAlertantModal",
+        // {{textos.editAlertant[idioma]}}
+        title: "EDITAR ALERTANTE",
+        info: {
+          name: "",
+          adress: "",
+          tel: ""
         }
+      },
+      currentAlertant: [],
+      filter: null
+    };
+  },
+  created() {
+    this.showAlertants();
+  },
+  computed: {
+    rows() {
+      return this.alertants.length;
     }
+  },
+  methods: {
+    showAlertants() {
+      let me = this;
+
+      axios
+        .get("/alertants")
+        .then(function(response) {
+          me.alertants = response.data;
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    },
+
+    deleteAlertant(item) {
+      let me = this;
+      axios
+        .delete("/alertants/" + item.id)
+        .then(function(response) {
+          me.showAlertants();
+        })
+        .catch(function(error) {
+          me.missatge = error.response.data;
+          me.mensajesError.push(me.missatge.error);
+        });
+    },
+
+    editAlertant(item) {
+      let me = this;
+      me.currentAlertant = item;
+
+      this.editAlertantModal.info.name = item.nom;
+      this.editAlertantModal.info.adress = item.adreca;
+      this.editAlertantModal.info.tel = item.telefon;
+      this.$bvModal.show("editAlertantModal");
+    },
+
+    saveAlertant() {
+      let me = this;
+
+      axios
+        .put("/alertants/" + me.currentAlertant.id, me.editAlertantModal.info)
+        .then(function(response) {
+          me.showAlertants();
+        })
+        .catch(function(error) {
+          me.missatge = error.response.data;
+          me.mensajesError.push(me.missatge.error);
+        });
+    }
+  },
+  mounted() {
+    console.log("Component mounted.");
+  }
+};
 </script>
